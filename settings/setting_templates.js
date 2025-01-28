f8d3d508d3e89d0893c516b392fa90e9a01d4066
daniel-153 <danieltrin1664@gmail.com>
2025-01-27 21:43:01 -0700

    create a js module to store templates for all the settings

---
+++ settings/setting_templates.js
@@ -0,0 +1,54 @@
+export const addsub_operation_type = {
+    type: 'radio_buttons',
+    code_name: 'operation_type',
+    display_name: 'Operation',
+    radio_buttons: [['add','Addition'],['subtract','Subtraction'],['both','Both']], 
+    tooltip: 'What operation should be between the numbers?'
+};
+
+export const muldiv_operation_type = {
+    type: 'radio_buttons',
+    code_name: 'operation_type',
+    display_name: 'Operation',
+    radio_buttons: [['multiply','Multiplication'],['divide','Division'],['both','Both']], 
+    tooltip: 'What operation should be between the numbers?'
+};
+
+export const answer_form = {
+    type: 'radio_buttons',
+    code_name: 'answer_form',
+    display_name: 'Answer Form',
+    radio_buttons: [['factions & integers','Factions & Integers'],['whole part + remainder','Whole Part + Remainder']], 
+    tooltip: 'What form should the answer be in?'
+};
+
+export const multiply_symbol = {
+    type: 'radio_buttons',
+    code_name: 'multiply_symbol',
+    display_name: 'Multiply Symbol',
+    radio_buttons: [[' \\cdot ','Dot a &middot; b'],[' \\times ','Cross a &times; b']], 
+    tooltip: 'Which multiplication symbol should be used?'
+};
+
+export const number_type = {
+    type: 'radio_buttons',
+    code_name: 'number_type',
+    display_name: 'Number Type',
+    radio_buttons: [['integers','Integers'],['fractions','Fractions'],['both','Both']], 
+    tooltip: 'What type of numbers should be in the expression?'
+};
+
+export const number_of_terms = {
+    type: 'single_textbox',
+    code_name: 'number_of_terms',
+    display_name: 'Number of Terms',
+    tooltip: 'How many numbers should be in the expression? (enter an integer from 2 to 10)'
+};
+
+export const term_range = {
+    type: 'range_textboxes',
+    code_names: ['term_range_min','term_range_max'],
+    display_name: 'Number Range',
+    tooltip: 'How big or small should the numbers be? (create a range with integers within ±999)'
+};
+
