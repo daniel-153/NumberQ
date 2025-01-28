b7eadc8ab727290c3dfa9d8496c622bcae6782d5
daniel-153 <danieltrin1664@gmail.com>
2025-01-27 22:41:10 -0700

    extract settings valadition to external functions

--- scripts/modules_w-sets/genAddSub.js
+++ scripts/modules_w-sets/genAddSub.js
@@ -1,93 +1,18 @@
 import * as H from '../helper-modules/gen-helpers.js';
+import * as SH from '../helper-modules/settings-helpers.js';
 
 function processSettings(formObj) {
-    let number_of_terms = formObj.number_of_terms;
-    let term_range_min = formObj.term_range_min;
-    let term_range_max = formObj.term_range_max;
-    let operation_type = formObj.operation_type;
+    let { number_of_terms, term_range_min, term_range_max, operation_type } = formObj;
     let error_locations = []; // stores a list of input fields where errors occures (same field can appear multiple times)
 
-    // validate number_of_terms (can be extracted to an external function)
-    if (number_of_terms === '') {
-        number_of_terms = 2;
-        error_locations.push('number_of_terms');
-    } // Number of terms has no input value
-    else number_of_terms = Number(number_of_terms);
-
-    if (Number.isNaN(number_of_terms)) {
-        number_of_terms = 2;
-        error_locations.push('number_of_terms');
-    } // number of terms is not a number (can't be converted to a number)
-    else if (number_of_terms > 10) {
-        number_of_terms = 10;
-        error_locations.push('number_of_terms');
-    } // Number of terms exceeds the max of the range
-    else if (number_of_terms < 2) {
-        number_of_terms = 2;
-        error_locations.push('number_of_terms');
-    } // Number of terms is below the mix of the range
-    else if (number_of_terms >= 2 && number_of_terms <= 10) {
-        if (!Number.isInteger(number_of_terms)) {
-            number_of_terms = Math.floor(Math.abs(number_of_terms));
-            error_locations.push('number_of_terms');
-        }
-    } // number of terms is in the correct range but isn't an integer
+    // validate number_of_terms and keep track of error locations
+    number_of_terms = SH.val_term_number(number_of_terms, error_locations);
     
 
-    // validate term range (can be extracted to an external function)
-    term_range_min = Number(term_range_min);
-    term_range_max = Number(term_range_max);
-    if (Number.isNaN(term_range_min) && Number.isNaN(term_range_max)) { // After this, term range min and max must be numbers
-        term_range_min = -10;
-        term_range_max = 10;
-        error_locations.push('term_range_min','term_range_max');
-    }
-    else if (Number.isNaN(term_range_min)) {
-        term_range_min  = -10;
-        error_locations.push('term_range_min');
-    }
-    else if (Number.isNaN(term_range_max)) {
-        term_range_max = 10;
-        error_locations.push('term_range_max');
-    }
-
-    if (!Number.isInteger(term_range_min)) { // After this, term range min and max must be integers
-        term_range_min = Math.floor(term_range_min);
-        error_locations.push('term_range_min');
-    }
-    if (!Number.isInteger(term_range_max)) {
-        term_range_max = Math.floor(term_range_max);
-        error_locations.push('term_range_max');
-    }
-
-    if (term_range_min < -999) { // After this |term range max/min| <= 999 (as stated in the tooltip requirement)
-        term_range_min = -999;
-        error_locations.push('term_range_min');
-    }
-    else if (term_range_min > 999) {
-        term_range_min = 999;
-        error_locations.push('term_range_min');
-    }
-    if (term_range_max > 999) {
-        term_range_max = 999;
-        error_locations.push('term_range_max');
-    }
-    else if (term_range_max < -999) {
-        term_range_max = -999;
-        error_locations.push('term_range_max');
-    }
-
-    if (term_range_min > term_range_max) { // After this, (term range min) <= (term range max)
-        let temp = term_range_min;
-        term_range_min = term_range_max;
-        term_range_max = temp;
-        error_locations.push('term_range_min','term_range_max');
-    }
-    if (term_range_min === 0 && term_range_max ===0) {
-        term_range_min = -10;
-        term_range_max = 10;
-        error_locations.push('term_range_min','term_range_max');
-    }
+    // validate the term range and keep track of error locations
+    let validatedMinMax = SH.val_min_max_range(term_range_min, term_range_max, error_locations);
+    term_range_min = validatedMinMax.term_range_min;
+    term_range_max = validatedMinMax.term_range_max;
 
     return {
         number_of_terms: number_of_terms,
