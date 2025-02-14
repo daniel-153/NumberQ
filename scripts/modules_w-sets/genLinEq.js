8a38b6769762bb88aade3c2c6fff531af6257a0e
daniel-153 <danieltrin1664@gmail.com>
2025-02-13 17:16:30 -0700

    begin creating an object to store equation templates and reqs

--- scripts/modules_w-sets/genLinEq.js
+++ scripts/modules_w-sets/genLinEq.js
@@ -31,4 +31,89 @@ function searchSols() {
     console.table(solutions);
 }
 
-searchSols();
+
+// pickEq(veryify_reqs(), get_sol(), create_prompt())
+
+// Global convention: in the verify_reqs function, the conditions are every way that a solution could fail. In other words, you are saying
+// 'If this, or this, or this, or this, or this is true, the set of numbers won't work' -> then you negate that to turn it into && 
+
+const equation_templates = {
+    begin_1: {
+        verify_reqs(a, b) {
+            return !(
+                a === 1 ||
+                !Number.isInteger(this.get_sol(a,b).raw_value)
+            );
+        },
+        get_sol(a,b) {
+            return {
+                raw_value: b / a,
+                numer: b,
+                denom: a
+            };
+        },
+        create_prompt(a, b) {
+            if (a === -1) a = '-'
+            return a + 'x=' + b;
+        },
+        number_of_coefs: 2
+    },
+    inter_1: {
+        verify_reqs(a, b, c, d) {
+            return !(
+                a === c ||
+                !Number.isInteger(this.get_sol(a,b,c,d).raw_value)
+            );
+        },
+        get_sol(a, b, c, d) {
+            return {
+                raw_value: (d - b) / (a - c),
+                numer: d - b,
+                denom: a - c
+            };
+        },
+        create_prompt(a, b, c, d) {
+            if (a === 1) a = '';
+            else if (a === -1) a = '-';
+
+            if (c === 1) c = '';
+            else if (c === -1) a = '-'; 
+
+            if (b > 0) b = '+' + b;
+            if (d > 0) d = '+' + d;
+
+            return a + 'x' + b + '=' + c + 'x' + d;
+        },
+        number_of_coefs: 4
+    },
+    advan_1: {
+        verify_reqs(a, b, c, d, e, f, g) {
+            return !(
+                b === 1,
+                e === 1,
+                (b * c) === (e * f) ||
+                !Number.isInteger(this.get_sol(a,b,c,d,e,f,g).raw_value)
+            );
+        },
+        get_sol(a,b) {
+            return {
+                raw_value: ((e*g) - a - (b*d)) / ((b*c) - (e*f)),
+                numer: ((e*g) - a - (b*d)),
+                denom: ((b*c) - (e*f))
+            };
+        },
+        create_prompt(a, b) {
+            if (b === 1) b = '+';
+            else if (b === -1) b = '-';
+            else if (b > 0) b = '+' + b;
+
+            // maybe add helper functions? you're going to need to repeat the above like 100 times...
+
+
+
+            return a + b + '(' + c + 'x' + d + ')=' + e +'(' + f + 'x' + g + ')';
+        },
+        number_of_coefs: 7
+    },
+
+};
