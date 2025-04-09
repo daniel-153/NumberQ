cd63c1fd34be195f683859ced7355905cf58e016
daniel-153 <danieltrin1664@gmail.com>
2025-04-09 15:49:28 -0600

    modify item insertion so item_ID and styles are stored on the worksheet element

--- scripts/worksheet/worksheet.js
+++ scripts/worksheet/worksheet.js
@@ -71,6 +71,41 @@ function getItemById(item_ID) {
     }
 }
 
+function getIdByItem(item_obj) {
+    let id_found = false;
+    if ('pages' in item_obj) {
+        if (item_obj === worksheet) {
+            id_found = true;
+            return 'document';
+        }
+    }
+    else if ('content' in item_obj) {
+        while (!id_found) {
+            for (let i = 0; i < worksheet.pages.length; i++) {
+                if (item_obj === worksheet.pages[i]) { // check for strict (reference) object equality (same obj)
+                    id_found = true;
+                    return 'page-' + i;
+                }
+            }
+        }
+    }
+    else {
+        while (!id_found) {
+            for (let i = 0; i < worksheet.pages.length; i++) {
+                for (let j = 0; j < worksheet.pages[i].content.length; j++) {
+                    if (item_obj === worksheet.pages[i].content[j]) {
+                        id_found = true;
+                        return 'content-' + i +'.' + j;
+                    }
+                }
+            }
+        }
+    }
+
+    // if we made it past the if-else above, no item matching item_obj was found
+    console.error(`item_object=${item_obj} was not found.`);
+}
+
 function appendItemAt(item_ID) {
     const [ item_type, item_number ] = item_ID.split('-');
 
@@ -88,7 +123,8 @@ function appendItemAt(item_ID) {
     else if (item_type === 'page') {
         worksheet.pages[Number(item_number)].content.push({
             settings: {
-                text_content: '[insert]'
+                text_content: '[insert]',
+                font_size: '1cm'
             },
         });
 
@@ -128,12 +164,12 @@ function editTextContent(item_ID, value) {
 
 // Rendering Functions:
 function render() {
-    function createProblemBoxHtml(problem_tex, problem_number,box_width) {
+    function createProblemBoxHtml(content_item, problem_number, box_width) {
         return `
-            <div class="content-box ${box_width}-width-box">
+            <div class="content-box ${box_width}-width-box" data-item-ID="${getIdByItem(content_item)}">
                 <div class="problem-number">${problem_number}.</div>
-                <div class="problem-tex">
-                    \\(${problem_tex}\\)
+                <div class="problem-tex" data-math-container="true" style="font-size: ${content_item.settings.font_size};">
+                    \\(${content_item.settings.text_content}\\)
                 </div>
             </div>
         `;
@@ -157,13 +193,13 @@ function render() {
         while (items_left > 0) {
             updated_html += `<div class="full-width-bounding-box">`;
             if (items_left > 1) {
-                updated_html += createProblemBoxHtml(worksheet.pages[i].content[current_item].settings.text_content, current_item + 1, 'half');
+                updated_html += createProblemBoxHtml(worksheet.pages[i].content[current_item], current_item + 1, 'half');
                 items_left--; current_item++;
-                updated_html += createProblemBoxHtml(worksheet.pages[i].content[current_item].settings.text_content, current_item + 1, 'half');
+                updated_html += createProblemBoxHtml(worksheet.pages[i].content[current_item], current_item + 1, 'half');
                 items_left--; current_item++;
             }
             else if (items_left === 1) {
-                updated_html += createProblemBoxHtml(worksheet.pages[i].content[current_item].settings.text_content, current_item + 1, 'half');
+                updated_html += createProblemBoxHtml(worksheet.pages[i].content[current_item], current_item + 1, 'half');
                 items_left--; current_item++;
             }
             updated_html += `</div>`
@@ -175,6 +211,24 @@ function render() {
     updated_html += '<div id="worksheet-preview-bottom">&nbsp;</div>'
     document.getElementById('worksheet-page-column').innerHTML = updated_html;
     MathJax.typesetPromise(['#worksheet-page-column']);
+}
+
+function fitWorksheetOverflow() {
+    function fitMathToElementCM(element) {
+        let scale_factor = element.clientWidth / element.scrollWidth;
+
+
+
+
+    }
+    
+    
+    [...document.getElementById('worksheet-page-content-area').children].forEach(block_bounding_box => {
+        [...block_bounding_box.children].forEach(content_box => {
+            const math_div = content_box.lastChild;
+
+        });
+    });
 }
 
 function updateOutline() {
