dfa42f696367f2bd93784b5b96c707e941a39ff2
daniel-153 <danieltrin1664@gmail.com>
2025-06-26 00:35:14 -0600

    add short names (for file downloads) on all gens

--- scripts/templates/topic-banners.js
+++ scripts/templates/topic-banners.js
@@ -4,21 +4,24 @@ export const templates = [
         category_class_name: 'arithmetic-gen',
         display_name: 'Addition & Subtraction',
         display_category: 'arithmetic',
-        example_problem: '4-6+8-5'
+        example_problem: '4-6+8-5',
+        short_name: 'add-subtract'
     },
     {
         function_name: 'genMulDiv',
         category_class_name: 'arithmetic-gen',
         display_name: 'Multiplication & Division',
         display_category: 'arithmetic',
-        example_problem: '4\\cdot\\frac{1}{6}\\cdot3\\cdot\\frac{1}{2}' 
+        example_problem: '4\\cdot\\frac{1}{6}\\cdot3\\cdot\\frac{1}{2}',
+        short_name: 'multiply-divide' 
     },
     {
         function_name: 'genAddFrac',
         category_class_name: 'arithmetic-gen',
         display_name: 'Adding Fractions',
         display_category: 'arithmetic',
-        example_problem: '\\frac{3}{4}+\\frac{2}{5}'
+        example_problem: '\\frac{3}{4}+\\frac{2}{5}',
+        short_name: 'fraction'
     },
     {
         function_name: 'genOrdOp',
@@ -26,7 +29,8 @@ export const templates = [
         display_name: 'Order of Operations',
         display_category: 'arithmetic',
         example_problem: '3\\times\\left(5+2^{3}\\right)-8\\div4',
-        example_problem_class: 'shrunk-question'
+        example_problem_class: 'shrunk-question',
+        short_name: 'pemdas'
     },
     {
         function_name: 'genLinEq',
@@ -34,14 +38,16 @@ export const templates = [
         display_name: 'Linear Equations',
         display_category: 'algebra',
         example_problem: '3x+2=14-9x',
-        example_problem_class: 'slightly-shrunk-question' 
+        example_problem_class: 'slightly-shrunk-question',
+        short_name: 'equation' 
     },
     {
         function_name: 'genFacQuad',
         category_class_name: 'algebra-gen',
         display_name: 'Quadratics & Factoring',
         display_category: 'algebra',
-        example_problem: '8x^{2}+10x+3' 
+        example_problem: '8x^{2}+10x+3',
+        short_name: 'quadratic' 
     },
     {
         function_name: 'genSysEqs',
@@ -55,35 +61,40 @@ export const templates = [
                         x + 5 = 4y
                         \\end{aligned}
                         `,
-        example_problem_class: 'shrunk-question'
+        example_problem_class: 'shrunk-question',
+        short_name: 'system'
     },
     {
         function_name: 'genSimRad',
         category_class_name: 'algebra-gen',
         display_name: 'Simplify Radicals',
         display_category: 'algebra',
-        example_problem: '\\sqrt{27}+\\sqrt{75}'
+        example_problem: '\\sqrt{27}+\\sqrt{75}',
+        short_name: 'radical'
     },
     {
         function_name: 'genPyTheo',
         category_class_name: 'geometry-gen',
         display_name: 'Pythagorean Theorem',
         display_category: 'geometry',
-        example_problem: 'a^{2}+b^{2}=c^{2}'
+        example_problem: 'a^{2}+b^{2}=c^{2}',
+        short_name: 'pythagorean'
     },
     {
         function_name: 'genTrigEx',
         category_class_name: 'precalc-gen',
         display_name: 'Trig Values',
         display_category: 'pre-calculus',
-        example_problem: '\\cos\\left(\\frac{4\\pi}{3}\\right)'
+        example_problem: '\\cos\\left(\\frac{4\\pi}{3}\\right)',
+        short_name: 'trig-value'
     },
     {
         function_name: 'genRatEx',
         category_class_name: 'precalc-gen',
         display_name: 'Rational Expressions',
         display_category: 'pre-calculus',
-        example_problem: '\\frac{x+2}{x-3}+\\frac{3x+1}{5x}'
+        example_problem: '\\frac{x+2}{x-3}+\\frac{3x+1}{5x}',
+        short_name: 'rational'
     },
     {
         function_name: 'genPolArith',
@@ -91,14 +102,16 @@ export const templates = [
         display_name: 'Polynomial Arithmetic',
         display_category: 'pre-calculus',
         example_problem: '(x^{2}+3x+2)(x+4)',
-        example_problem_class: 'shrunk-shrunk-question'
+        example_problem_class: 'shrunk-shrunk-question',
+        short_name: 'polynomial'
     },
     {
         function_name: 'genComArith',
         category_class_name: 'precalc-gen',
         display_name: 'Complex Arithmetic',
         display_category: 'pre-calculus',
-        example_problem: '\\frac{6+8i}{2-4i}'
+        example_problem: '\\frac{6+8i}{2-4i}',
+        short_name: 'complex-number'
     },
     {
         function_name: 'genVecOp',
@@ -109,7 +122,8 @@ export const templates = [
                             \\left\\lVert \\vec{v} \\right\\rVert \\:\\;\\, 
                             c\\vec{v} \\:\\;\\, 
                             \\frac{\\vec{v}}{\\left\\lVert \\vec{v} \\right\\rVert}`,
-        example_problem_class: 'slightly-shrunk-question'
+        example_problem_class: 'slightly-shrunk-question',
+        short_name: 'vector'
     },
     {
         function_name: 'genVecArith',
@@ -124,7 +138,8 @@ export const templates = [
                             5\\\\
                             2
                           \\end{bmatrix}`,
-        example_problem_class: 'shrunk-3x-question'
+        example_problem_class: 'shrunk-3x-question',
+        short_name: 'vector'
     },
     {
         function_name: 'genMtrxOp',
@@ -135,7 +150,8 @@ export const templates = [
                             \\operatorname{det}(A) \\:\\;\\, 
                             A^{-1} \\:\\;\\, 
                             \\operatorname{rref}(A)`,
-        example_problem_class: 'shrunk-question'
+        example_problem_class: 'shrunk-question',
+        short_name: 'matrix'
     },
     {
         function_name: 'genMtrxArith',
@@ -150,7 +166,8 @@ export const templates = [
                             2 & 4 \\\\
                             1 & 6
                           \\end{bmatrix}`,
-        example_problem_class: 'shrunk-3x-question'
+        example_problem_class: 'shrunk-3x-question',
+        short_name: 'matrix'
     }
 ];
 
