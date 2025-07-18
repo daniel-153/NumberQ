9466fd281678c3b428caaf8bc7206f5d3c40c559
daniel-153 <danieltrin1664@gmail.com>
2025-07-17 21:57:24 -0600

    add to test genOrdOp

---
+++ testing/gen-testing/gen-verifiers/verOrdOp.py
@@ -0,0 +1,13 @@
+from sympy import simplify
+from sympy.parsing.latex import parse_latex
+
+def verify(tex_question, tex_answer): 
+    question = parse_latex(tex_question)
+    answer = parse_latex(tex_answer)
+
+    calculated_answer = simplify(question)
+
+    if answer == calculated_answer:
+        return None # indicate success (no wrong answer to log)
+    else:
+        return calculated_answer # answer that didn't match
