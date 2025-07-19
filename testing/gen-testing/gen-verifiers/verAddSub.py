f48380bd09bf26a1339478b600cb2cfbce873a16
daniel-153 <danieltrin1664@gmail.com>
2025-07-19 01:32:04 -0600

    update to reflect change to tex question and answer (always strings across all gens in testing)

--- testing/gen-testing/gen-verifiers/verAddSub.py
+++ testing/gen-testing/gen-verifiers/verAddSub.py
@@ -1,12 +1,12 @@
-from sympy import simplify
 from sympy.parsing.latex import parse_latex
 
-def verify(tex_question, tex_answer): 
+def verify(tex_question, tex_answer):
     question = parse_latex(tex_question)
+    answer = parse_latex(tex_answer)
 
-    calculated_answer = simplify(question)
+    calculated_answer = question.doit()
 
-    if tex_answer == calculated_answer:
+    if answer == calculated_answer:
         return None # indicate success (no wrong answer to log)
     else:
         return calculated_answer # answer that didn't match
