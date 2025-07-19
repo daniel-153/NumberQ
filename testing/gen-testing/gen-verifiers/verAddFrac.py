b28408e98d7a32503c249b1bbc3be8b12c7c9fef
daniel-153 <danieltrin1664@gmail.com>
2025-07-19 12:38:18 -0600

    add a verifier for genAddFrac

---
+++ testing/gen-testing/gen-verifiers/verAddFrac.py
@@ -0,0 +1,22 @@
+from sympy.parsing.latex import parse_latex
+
+def verify(tex_question, tex_answer):
+    question = parse_latex(tex_question)
+
+    # determine if the answer is in fraction, integer, or mixed number form
+    answer = None
+    if tex_answer.startswith('\\frac'): # is a fraction
+        answer = parse_latex(tex_answer)
+    elif '\\frac' in tex_answer: # contains a fraction but not at the start (mixed number)
+        just_before_frac = tex_answer.index('\\')
+        parsed_mixed_number = tex_answer[:just_before_frac] + '+' + tex_answer[just_before_frac:] # 3\frac{2}{5} -> 3 + \frac{2}{5}
+        answer = parse_latex(parsed_mixed_number)
+    else: # doesn't contain a fraction (just an integer)
+        answer = parse_latex(tex_answer)
+
+    calculated_answer = question.doit()
+
+    if answer.doit() == calculated_answer:
+        return None
+    else:
+        return calculated_answer
