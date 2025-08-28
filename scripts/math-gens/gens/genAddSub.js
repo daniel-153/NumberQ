9247506e817f88a1e2227fb8c92bdb4e420db94f
daniel-153 <danieltrin1664@gmail.com>
2025-08-27 21:45:42 -0600

    add full preset list

--- scripts/math-gens/gens/genAddSub.js
+++ scripts/math-gens/gens/genAddSub.js
@@ -175,17 +175,434 @@ export const presets = {
     },
     topic_presets: [
         {
-            name: 'one_digit_addition', 
-            display_title: 'One Digit Addition',
+            display_title: 'Addition Within 10',
             example_problem: '3+5=\\:?',
-            description: 'Add numbers between 0 and 9.',
+            description: 'Add numbers between 0 and 10.',
             get_settings: function() {
                 return {
                     first_term_range_min: 0,
-                    first_term_range_max: 9,
+                    first_term_range_max: 10,
+                    second_term_range_min: 0,
+                    second_term_range_max: 10,
+                    addsub_operation: 'add'
+                };
+            }
+        },
+        {
+            display_title: 'Addition Within 20',
+            example_problem: '13+17=\\:?',
+            description: 'Add numbers between 0 and 20.',
+            get_settings: function() {
+                return {
+                    first_term_range_min: 0,
+                    first_term_range_max: 20,
+                    second_term_range_min: 0,
+                    second_term_range_max: 20,
+                    addsub_operation: 'add'
+                };
+            }
+        },
+        {
+            display_title: 'Subtraction Within 10',
+            example_problem: '9-5=\\:?',
+            description: 'Subtract numbers between 0 and 10.',
+            get_settings: function() {
+                return {
+                    first_term_range_min: 0,
+                    first_term_range_max: 10,
+                    second_term_range_min: 0,
+                    second_term_range_max: 10,
+                    addsub_operation: 'subtract',
+                    force_non_neg_sub: 'yes'
+                };
+            }
+        },
+        {
+            display_title: 'Subtraction Within 20',
+            example_problem: '19-3=\\:?',
+            description: 'Subtract numbers between 0 and 20.',
+            get_settings: function() {
+                return {
+                    first_term_range_min: 0,
+                    first_term_range_max: 20,
+                    second_term_range_min: 0,
+                    second_term_range_max: 20,
+                    addsub_operation: 'subtract',
+                    force_non_neg_sub: 'yes'
+                };
+            }
+        },
+        {
+            display_title: 'Mixed Operations Within 10',
+            example_problem: '\\overset{[0,10]}{A} \\pm \\overset{[0,10]}{B}',
+            description: 'Add and subtract numbers within 10.',
+            get_settings: function() {
+                return {
+                    first_term_range_min: 0,
+                    first_term_range_max: 10,
+                    second_term_range_min: 0,
+                    second_term_range_max: 10,
+                    addsub_operation: 'either',
+                    force_non_neg_sub: 'yes'
+                };
+            }
+        },
+        {
+            display_title: 'Mixed Operations Within 20',
+            example_problem: '\\overset{[0,20]}{A} \\pm \\overset{[0,20]}{B}',
+            description: 'Add and subtract numbers within 20.',
+            get_settings: function() {
+                return {
+                    first_term_range_min: 0,
+                    first_term_range_max: 20,
+                    second_term_range_min: 0,
+                    second_term_range_max: 20,
+                    addsub_operation: 'either',
+                    force_non_neg_sub: 'yes'
+                };
+            }
+        },
+        {
+            display_title: 'Addition With Negatives',
+            example_problem: '(-3)+(-2)=\\:?',
+            description: 'Add (A + B) where one or both of A and B are negative numbers.',
+            get_settings: function() {
+                let [first_term_is_neg, second_term_is_neg] = [H.randFromList([true, false]), H.randFromList([true, false])];
+                if (H.randInt(0, 1) === 0 && !first_term_is_neg) second_term_is_neg = true;
+                else if (!second_term_is_neg) first_term_is_neg = true;
+                
+                const first_term_range = (first_term_is_neg)? [-9, -1] : [0, 10];
+                const second_term_range = (second_term_is_neg)? [-9, -1] : [0, 10];
+
+                return {
+                    first_term_range_min: first_term_range[0],
+                    first_term_range_max: first_term_range[1],
+                    second_term_range_min: second_term_range[0],
+                    second_term_range_max: second_term_range[1],
+                    addsub_operation: 'add'
+                };
+            }
+        },
+        {
+            display_title: 'Subtraction With Negatives',
+            example_problem: '(-8)-(-7)=\\:?',
+            description: 'Subtract (A - B) where one or both of A and B are negative numbers.',
+            get_settings: function() {
+                let [first_term_is_neg, second_term_is_neg] = [H.randFromList([true, false]), H.randFromList([true, false])];
+                if (H.randInt(0, 1) === 0 && !first_term_is_neg) second_term_is_neg = true;
+                else if (!second_term_is_neg) first_term_is_neg = true;
+                
+                const first_term_range = (first_term_is_neg)? [-9, -1] : [0, 10];
+                const second_term_range = (second_term_is_neg)? [-9, -1] : [0, 10];
+
+                return {
+                    first_term_range_min: first_term_range[0],
+                    first_term_range_max: first_term_range[1],
+                    second_term_range_min: second_term_range[0],
+                    second_term_range_max: second_term_range[1],
+                    addsub_operation: 'subtract'
+                };
+            }
+        },
+        {
+            display_title: 'Addition With Zero',
+            example_problem: 'A+0=\\:?',
+            description: 'Add a number and zero.',
+            get_settings: function() {
+                let first_term_max, second_term_max;
+                if (H.randInt(0, 1) === 0) {
+                    first_term_max = 0;
+                    second_term_max = Number('9' + H.randFromList(['', '9', '99']));
+                }
+                else {
+                    first_term_max = Number('9' + H.randFromList(['', '9', '99']));
+                    second_term_max = 0;
+                }
+                
+                return {
+                    first_term_range_min: 0,
+                    first_term_range_max: first_term_max,
+                    second_term_range_min: 0,
+                    second_term_range_max: second_term_max,
+                    addsub_operation: 'add',
+                    allow_zero_terms: 'yes'
+                };
+            }
+        },
+        {
+            display_title: 'Subtraction With Zero',
+            example_problem: 'A-0=\\:?',
+            description: 'Subtract zero from a number.',
+            get_settings: function() {    
+                return {
+                    first_term_range_min: 0,
+                    first_term_range_max: Number('9' + H.randFromList(['', '9', '99'])),
+                    second_term_range_min: 0,
+                    second_term_range_max: 0,
+                    addsub_operation: 'subtract',
+                    allow_zero_terms: 'yes'
+                };
+            }
+        },
+        {
+            display_title: 'Mixed Operations With Negatives',
+            example_problem: 'A\\pm B,~~A \\text{ or } B \\lt 0',
+            description: 'Add or subtract two numbers where at least one number is negative.',
+            get_settings: function() {
+                let [first_term_is_neg, second_term_is_neg] = [H.randFromList([true, false]), H.randFromList([true, false])];
+                if (H.randInt(0, 1) === 0 && !first_term_is_neg) second_term_is_neg = true;
+                else if (!second_term_is_neg) first_term_is_neg = true;
+                
+                const first_term_range = (first_term_is_neg)? [-9, -1] : [0, 10];
+                const second_term_range = (second_term_is_neg)? [-9, -1] : [0, 10];
+
+                return {
+                    first_term_range_min: first_term_range[0],
+                    first_term_range_max: first_term_range[1],
+                    second_term_range_min: second_term_range[0],
+                    second_term_range_max: second_term_range[1],
+                    addsub_operation: 'either'
+                };
+            }
+        },
+        {
+            display_title: 'Subtraction With Negative Results',
+            example_problem: 'A-B\\lt 0',
+            description: 'Subtract two numbers A and B where the result (A&thinsp;-&thinsp;B) is negative.',
+            get_settings: function() {
+                const term_1 = H.randInt(0, 19);
+                const term_2 = H.randInt(term_1 + 1, 20);
+
+                return {
+                    first_term_range_min: term_1,
+                    first_term_range_max: term_1,
+                    second_term_range_min: term_2,
+                    second_term_range_max: term_2,
+                    addsub_operation: 'subtract',
+                    force_non_neg_sub: 'no'
+                };
+            }
+        },
+        {
+            display_title: 'Add 1 Digit to 2 Digits',
+            example_problem: '\\begin{array}{@{}r@{}} 32\\\\[-0.4em] \\underline{\\smash[b]{+~\\phantom{3}8}} \\end{array}',
+            description: 'Add a number with one digit to a number with two digits.',
+            get_settings: function() {
+                return {
+                    first_term_range_min: 10,
+                    first_term_range_max: 99,
                     second_term_range_min: 0,
                     second_term_range_max: 9,
-                    addsub_operation: 'add'
+                    addsub_operation: 'add',
+                    addsub_notation: 'stacked'
+                };
+            }
+        },
+        {
+            display_title: 'Add 2 Digits to 2 Digits',
+            example_problem: '\\begin{array}{@{}r@{}} 84\\\\[-0.4em] \\underline{\\smash[b]{+~25}} \\end{array}',
+            description: 'Add two numbers that each have two digits.',
+            get_settings: function() {
+                return {
+                    first_term_range_min: 10,
+                    first_term_range_max: 99,
+                    second_term_range_min: 10,
+                    second_term_range_max: 99,
+                    addsub_operation: 'add',
+                    addsub_notation: 'stacked'
+                };
+            }
+        },
+        {
+            display_title: 'Mixed Addition Within 2 Digits',
+            example_problem: '\\overset{[0,99]}{A} + \\overset{[0,99]}{B}',
+            description: 'Add numbers that have either one or two digits.',
+            get_settings: function() {
+                return {
+                    first_term_range_min: 0,
+                    first_term_range_max: Number('9' + H.randFromList(['', '9'])),
+                    second_term_range_min: 0,
+                    second_term_range_max: Number('9' + H.randFromList(['', '9'])),
+                    addsub_operation: 'add',
+                    addsub_notation: 'stacked'
+                };
+            }
+        },
+        {
+            display_title: 'Mixed Addition Within 3 Digits',
+            example_problem: '\\overset{[0,999]}{A} + \\overset{[0,999]}{B}',
+            description: 'Add numbers that have between one and three digits.',
+            get_settings: function() {
+                return {
+                    first_term_range_min: 0,
+                    first_term_range_max: Number('9' + H.randFromList(['', '9', '99'])),
+                    second_term_range_min: 0,
+                    second_term_range_max: Number('9' + H.randFromList(['', '9', '99'])),
+                    addsub_operation: 'add',
+                    addsub_notation: 'stacked'
+                };
+            }
+        },
+        {
+            display_title: 'Subtract 1 Digit from 2 Digits',
+            example_problem: '\\begin{array}{@{}r@{}} 46\\\\[-0.4em] \\underline{\\smash[b]{-~\\phantom{3}7}} \\end{array}',
+            description: 'Subtract a number with one digit from a number with two digits.',
+            get_settings: function() {
+                return {
+                    first_term_range_min: 10,
+                    first_term_range_max: 99,
+                    second_term_range_min: 0,
+                    second_term_range_max: 9,
+                    addsub_operation: 'subtract',
+                    addsub_notation: 'stacked',
+                    force_non_neg_sub: 'yes'
+                };
+            }
+        },
+        {
+            display_title: 'Subtract 2 Digits from 2 Digits',
+            example_problem: '\\begin{array}{@{}r@{}} 91\\\\[-0.4em] \\underline{\\smash[b]{-~17}} \\end{array}',
+            description: 'Subtract a two digit number from another two digit number.',
+            get_settings: function() {
+                return {
+                    first_term_range_min: 10,
+                    first_term_range_max: 99,
+                    second_term_range_min: 10,
+                    second_term_range_max: 99,
+                    addsub_operation: 'subtract',
+                    addsub_notation: 'stacked',
+                    force_non_neg_sub: 'yes'
+                };
+            }
+        },
+        {
+            display_title: 'Mixed Subtraction Within 2 Digits',
+            example_problem: '\\overset{[0,99]}{A} - \\overset{[0,99]}{B}',
+            description: 'Subtract numbers that have either one or two digits.',
+            get_settings: function() {
+                return {
+                    first_term_range_min: 0,
+                    first_term_range_max: Number('9' + H.randFromList(['', '9'])),
+                    second_term_range_min: 0,
+                    second_term_range_max: Number('9' + H.randFromList(['', '9'])),
+                    addsub_operation: 'subtract',
+                    addsub_notation: 'stacked',
+                    force_non_neg_sub: 'yes'
+                };
+            }
+        },
+        {
+            display_title: 'Mixed Subtraction Within 3 Digits',
+            example_problem: '\\overset{[0,999]}{A} - \\overset{[0,999]}{B}',
+            description: 'Subtract numbers that have between one and three digits.',
+            get_settings: function() {
+                return {
+                    first_term_range_min: 0,
+                    first_term_range_max: Number('9' + H.randFromList(['', '9', '99'])),
+                    second_term_range_min: 0,
+                    second_term_range_max: Number('9' + H.randFromList(['', '9', '99'])),
+                    addsub_operation: 'subtract',
+                    addsub_notation: 'stacked',
+                    force_non_neg_sub: 'yes'
+                };
+            }
+        },
+        {
+            display_title: 'Mixed Operations Within 3 Digits',
+            example_problem: '\\overset{[0,999]}{A} \\pm \\overset{[0,999]}{B}',
+            description: 'Add or subtract numbers that have between one and three digits.',
+            get_settings: function() {
+                return {
+                    first_term_range_min: 0,
+                    first_term_range_max: Number('9' + H.randFromList(['', '9', '99'])),
+                    second_term_range_min: 0,
+                    second_term_range_max: Number('9' + H.randFromList(['', '9', '99'])),
+                    addsub_operation: 'either',
+                    addsub_notation: 'stacked',
+                    force_non_neg_sub: 'yes'
+                };
+            }
+        },
+        {
+            display_title: 'Adding by Tens',
+            example_problem: 'A+(10k)=\\:?',
+            description: 'Add a number and a multiple of ten.',
+            get_settings: function() {
+                const ten_multiple = 10 * H.randInt(1, 9);
+                
+                return {
+                    first_term_range_min: 0,
+                    first_term_range_max: Number('9' + H.randFromList(['', '9', '99'])),
+                    second_term_range_min: ten_multiple,
+                    second_term_range_max: ten_multiple,
+                    addsub_operation: 'add',
+                    addsub_notation: 'stacked'
+                };
+            }
+        },
+        {
+            display_title: 'Adding by Hundreds',
+            example_problem: 'A+(100k)=\\:?',
+            description: 'Add a number and a multiple of 100.',
+            get_settings: function() {
+                const hundred_multiple = 100 * H.randInt(1, 9);
+
+                return {
+                    first_term_range_min: 0,
+                    first_term_range_max: Number('9' + H.randFromList(['', '9', '99'])),
+                    second_term_range_min: hundred_multiple,
+                    second_term_range_max: hundred_multiple,
+                    addsub_operation: 'add',
+                    addsub_notation: 'stacked'
+                };
+            }
+        },
+        {
+            display_title: 'Adding With Tens and Hundreds',
+            example_problem: '10+500=\\:?',
+            description: 'Add two numbers that are multiples of 10 or 100.',
+            get_settings: function() {
+                const term_1 = H.randInt(1, 9) * 10**(H.randInt(1, 2));
+                const term_2 = H.randInt(1, 9) * 10**(H.randInt(1, 2));
+
+                return {
+                    first_term_range_min: term_1,
+                    first_term_range_max: term_1,
+                    second_term_range_min: term_2,
+                    second_term_range_max: term_2,
+                    addsub_operation: 'add',
+                    addsub_notation: 'stacked'
+                };
+            }
+        },
+        {
+            display_title: 'Large Mixed Addition (1-4 digits)',
+            example_problem: '\\begin{array}{@{}r@{}} 5260\\\\[-0.4em] \\underline{\\smash[b]{+~\\phantom{5}478}} \\end{array}',
+            description: 'Add numbers with between one and four digits.',
+            get_settings: function() {
+                return {
+                    first_term_range_min: 0,
+                    first_term_range_max: Number('9' + H.randFromList(['', '9', '99', '999'])),
+                    second_term_range_min: 0,
+                    second_term_range_max: Number('9' + H.randFromList(['', '9', '99', '999'])),
+                    addsub_operation: 'add',
+                    addsub_notation: 'stacked'
+                };
+            }
+        },
+        {
+            display_title: 'Large Mixed Subtraction (1-4 digits)',
+            example_problem: '\\begin{array}{@{}r@{}} 5087\\\\[-0.4em] \\underline{\\smash[b]{-~\\phantom{50}71}} \\end{array}',
+            description: 'Subtract numbers with between one and four digits.',
+            get_settings: function() {
+                return {
+                    first_term_range_min: 0,
+                    first_term_range_max: Number('9' + H.randFromList(['', '9', '99', '999'])),
+                    second_term_range_min: 0,
+                    second_term_range_max: Number('9' + H.randFromList(['', '9', '99', '999'])),
+                    addsub_operation: 'subtract',
+                    force_non_neg_sub: 'yes',
+                    addsub_notation: 'stacked'
                 };
             }
         }
