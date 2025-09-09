fe1b2bd9333607aaf573ace34c99f83ec67ce70c
daniel-153 <danieltrin1664@gmail.com>
2025-09-08 23:18:30 -0600

    add partial preset set

--- scripts/math-gens/gens/genRatEx.js
+++ scripts/math-gens/gens/genRatEx.js
@@ -1228,7 +1228,142 @@ export const presets = {
             denom_form: 'factored',
             give_excluded_values: 'no'
         };
-    }
+    },
+    topic_presets: [
+        {
+            title: 'Addition And Subtraction (beginner)',
+            example_problem: '\\frac{1}{5x}+\\frac{1}{4x}',
+            description: 'Add and subtract with beginner forms.',
+            get_settings: function() {    
+                return {
+                    ratex_add_sub_form: `as_${H.randFromList([1,2,3,4,5,13])}`,
+                    general_operation_types: ['add','subtract']
+                };
+            }
+        },
+        {
+            title: 'Multiplication (beginner)',
+            example_problem: '\\frac{5}{2x}\\cdot\\frac{1}{3x}',
+            description: 'Multiply with beginner forms.',
+            get_settings: function() {    
+                return {
+                    ratex_mul_div_form: `md_${H.randInt(1, 7)}`,
+                    general_operation_types: ['multiply']
+                };
+            }
+        },
+        {
+            title: 'Division (beginner)',
+            example_problem: '\\frac{x-3}{x+3}\\div\\frac{x-4}{4}',
+            description: 'Divide with beginner forms.',
+            get_settings: function() {    
+                return {
+                    ratex_mul_div_form: `md_${H.randInt(1, 7)}`,
+                    general_operation_types: ['divide']
+                };
+            }
+        },
+        {
+            title: 'Any Operation (beginner)',
+            example_problem: `
+            \\begin{array}{c}
+                P(x)\\,[+-\\:\\cdot\\:\\div]\\,Q(x) \\\\
+                [\\mathrm{beginner}]
+            \\end{array}
+            `,
+            description: 'Random operations with beginner forms.',
+            get_settings: function() {    
+                return {
+                    ratex_add_sub_form: `as_${H.randFromList([1,2,3,4,5,13])}`,
+                    ratex_mul_div_form: `md_${H.randInt(1, 7)}`,
+                    general_operation_types: '__random__'
+                };
+            }
+        },
+        {
+            title: 'Addition And Subtraction (advanced)',
+            example_problem: '\\frac{2x+6}{2x-7}+\\frac{6x+4}{3x-6}',
+            description: 'Add and subtract with advanced forms.',
+            get_settings: function() {    
+                return {
+                    ratex_add_sub_form: `as_${H.randFromList(H.removeFromArray([1,2,3,4,5,13], H.integerArray(1, 15)))}`,
+                    general_operation_types: ['add','subtract']
+                };
+            }
+        },
+        {
+            title: 'Multiplication (advanced)',
+            example_problem: '\\frac{6x^{2}-5x}{7x^{2}-3x}\\cdot\\frac{x+4}{x+3}',
+            description: 'Multiply with advanced forms.',
+            get_settings: function() {    
+                return {
+                    ratex_mul_div_form: `md_${H.randInt(8, 16)}`,
+                    general_operation_types: ['multiply']
+                };
+            }
+        },
+        {
+            title: 'Division (advanced)',
+            example_problem: '\\frac{x}{2x^{2}+5x}\\div\\frac{x-2}{x+2}',
+            description: 'Divide with advanced forms.',
+            get_settings: function() {    
+                return {
+                    ratex_mul_div_form: `md_${H.randInt(8, 16)}`,
+                    general_operation_types: ['divide']
+                };
+            }
+        },
+        {
+            title: 'Any Operation (advanced)',
+            example_problem: `
+            \\begin{array}{c}
+                P(x)\\,[+-\\:\\cdot\\:\\div]\\,Q(x) \\\\
+                [\\mathrm{advanced}]
+            \\end{array}
+            `,
+            description: 'Random operations with advanced forms.',
+            get_settings: function() {    
+                return {
+                    ratex_add_sub_form: `as_${H.randFromList(H.removeFromArray([1,2,3,4,5,13], H.integerArray(1, 15)))}`,
+                    ratex_mul_div_form: `md_${H.randInt(8, 16)}`,
+                    general_operation_types: '__random__'
+                };
+            }
+        },
+        {
+            title: 'Addition And Subtraction (quadratic denominators)',
+            example_problem: '\\frac{-7}{4x^{2}-x}-\\frac{7}{2x}',
+            description: 'Add and subtract where at least one denominator has a degree of 2.',
+            get_settings: function() {    
+                return {
+                    ratex_add_sub_form: `as_${H.randFromList([8, 9, 10, 11, 12, 13, 15])}`,
+                    general_operation_types: ['add','subtract']
+                };
+            }
+        },
+        {
+            title: 'Addition And Subtraction (different denominator forms)',
+            example_problem: '\\frac{6}{6x+1}+\\frac{7}{3x}',
+            description: 'Add and subtract where forms of each denominator don\'t match.',
+            get_settings: function() {    
+                return {
+                    ratex_add_sub_form: `as_${H.randFromList([4, 7, 8, 9, 10, 11, 12, 13, 15])}`,
+                    general_operation_types: ['add','subtract']
+                };
+            }
+        },
+        {
+            title: 'Multiplication With Cancellation',
+            example_problem: '\\frac{6x^{2}-5x}{7x^{2}-3x}\\cdot\\frac{x+4}{x+3}',
+            description: 'Multiply where some common factors between the expressions will cancel.',
+            get_settings: function() {    
+                return {
+                    ratex_mul_div_form: `md_${H.randFromList([6, 8])}`,
+                    general_operation_types: ['multiply']
+                };
+            }
+        },
+    ]
 };
 
 export const size_adjustments = {
