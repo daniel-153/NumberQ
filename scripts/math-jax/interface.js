f084749f9df69f80e2999f0fa9894d0ed620180d
daniel-153 <danieltrin1664@gmail.com>
2025-09-18 17:10:13 -0600

    add function to mimic MathJax typesetPromise

--- scripts/math-jax/interface.js
+++ scripts/math-jax/interface.js
@@ -76,6 +76,60 @@ await (async function init() {
                     return img;
                 }));
             },
+            typesetPromise: async function(root_element = document.body, format = 'chtml', scale = 1) {
+                if (!(root_element instanceof Element)) {
+                    throw new Error('Supplied root element is not an html element.');
+                }
+                if (!['chtml', 'svg', 'img'].includes(format)) {
+                    throw new Error(`Supplied format '${format}' is invalid. Format must be 'chtml', 'svg', or 'img'.`);
+                }
+                if (Number.isNaN(scale) || scale <= 0) {
+                    throw new Error(`Supplied scale '${scale}' is invalid. Scale must be a positve number.`);
+                }
+
+                root_element.normalize();
+                const tree_walker = document.createTreeWalker(
+                    root_element,
+                    NodeFilter.SHOW_TEXT
+                );
+
+                const text_nodes = [];
+                let text_node;
+                while ((text_node = tree_walker.nextNode())) {
+                    text_nodes.push(text_node);
+                }
+                
+                let tex_strings = [];
+                const mjx_nodes = [];
+                const math_regex = /\\\(([.\s\S]+?)\\\)/; 
+                text_nodes.forEach(text_node => {
+                    let delim_match = text_node.nodeValue.match(math_regex);
+                    let node_slice = text_node;
+
+                    while (delim_match) {
+                        const math_node = node_slice.splitText(delim_match.index);
+                        node_slice = math_node.splitText(delim_match[0].length);
+
+                        tex_strings.push(math_node.nodeValue.slice(2, -2));
+                        mjx_nodes.push(math_node);
+
+                        delim_match = node_slice.nodeValue.match(math_regex);
+                    }
+                });
+                
+                if (format === 'svg' || format === 'img') {
+                    tex_strings = tex_strings.map(tex_str => [tex_str, scale]);
+                }
+
+                let math_output_els = await this.resolveTexFunc(format)(tex_strings);
+
+                for (let i = 0; i < mjx_nodes.length; i++) {
+                    const mjx_node = mjx_nodes[i];
+                    const math_el = math_output_els[i];
+
+                    mjx_node.parentNode.replaceChild(math_el, mjx_node);
+                }
+            },
 
             // implemented in get()
             loadSvgComponents: async function(component_paths) {},
@@ -83,75 +137,91 @@ await (async function init() {
         },
         {
             __task_queue__: Promise.resolve(),
+            __getTaskFunc__: function(func_name, mjx_loader) {
+                let task_func;
+                if (['texToChtml', 'texToSvg', 'texToImg'].includes(func_name)) {
+                    let typesetFunc, loader_name;
+                    if (func_name === 'texToChtml') {
+                        loader_name = 'chtml';
+                        typesetFunc = async function(tex_str_arr) {
+                            return await mjx_loader['texToChtml'].call({target_iframe: iframes['chtml']}, tex_str_arr)
+                        }
+                    }
+                    else if (func_name === 'texToSvg') {
+                        loader_name = 'svg';
+                        typesetFunc = async function(tex_str_arr) {
+                            return await mjx_loader['texToSvg'].call({target_iframe: iframes['svg']}, tex_str_arr)
+                        }
+                    }   
+                    else if (func_name === 'texToImg') {
+                        loader_name = 'svg';
+                        typesetFunc = async function(tex_str_arr) {
+                            return await mjx_loader['texToImg'].call({
+                                target_iframe: iframes['svg'],
+                                texToSvg: mjx_loader['texToSvg']
+                            }, tex_str_arr);
+                        }
+                    }
+
+                    task_func = async function(tex_str_arr) {
+                        const render_count = `${loader_name}_render_count`;
+                        const render_limit = `${loader_name}_render_limit`;
+                        const capacity = Math.max(0, mjx_loader[render_limit] - mjx_loader[render_count]);
+                        let pre_limit_tex = tex_str_arr.slice(0, capacity);
+                        let post_limit_tex = tex_str_arr.slice(capacity);
+                        let rendered_output_arr = []; 
+
+                        do {
+                            if (pre_limit_tex.length > 0) {
+                                rendered_output_arr = rendered_output_arr.concat(
+                                    await typesetFunc(pre_limit_tex)
+                                );
+
+                                mjx_loader[render_count] += pre_limit_tex.length;
+                                pre_limit_tex.length = 0
+                            }
+
+                            if (post_limit_tex.length > 0) {
+                                await destroyAndRestartLoader(loader_name);
+                                mjx_loader[render_count] = 0;
+                                
+                                pre_limit_tex = post_limit_tex.splice(0, Math.min(mjx_loader[render_limit], post_limit_tex.length));
+                            }
+                        } while (pre_limit_tex.length > 0);
+
+                        return rendered_output_arr;
+                    }
+                }
+                else if (['loadSvgComponents', 'loadChtmlComponents'].includes(func_name)) {
+                    const svg_or_chtml = func_name.slice(4, 9).toLowerCase();
+
+                    task_func = async function(component_paths) {
+                        await newIframeMessagePromise(iframes[svg_or_chtml], {
+                            message_type: 'load_mjx_components',
+                            component_paths: component_paths
+                        });
+
+                        if (!Array.isArray(iframes[svg_or_chtml].loaded_extensions)) {
+                            iframes[svg_or_chtml].loaded_extensions = [];
+                        }
+
+                        iframes[svg_or_chtml].loaded_extensions.push(...component_paths)
+                    }
+                }
+
+                return task_func;
+            },
             get: function(mjx_loader, accessed_prop_name) {
                 if (typeof(mjx_loader[accessed_prop_name]) === 'function') {
                     let current_task_func;
-                    if (['texToChtml', 'texToSvg', 'texToImg'].includes(accessed_prop_name)) {
-                        let typesetFunc, loader_name;
-                        if (accessed_prop_name === 'texToChtml') {
-                            loader_name = 'chtml';
-                            typesetFunc = async function(tex_str_arr) {
-                                return await mjx_loader['texToChtml'].call({target_iframe: iframes['chtml']}, tex_str_arr)
-                            }
-                        }
-                        else if (accessed_prop_name === 'texToSvg') {
-                            loader_name = 'svg';
-                            typesetFunc = async function(tex_str_arr) {
-                                return await mjx_loader['texToSvg'].call({target_iframe: iframes['svg']}, tex_str_arr)
-                            }
-                        }   
-                        else if (accessed_prop_name === 'texToImg') {
-                            loader_name = 'svg';
-                            typesetFunc = async function(tex_str_arr) {
-                                return await mjx_loader['texToImg'].call({
-                                    target_iframe: iframes['svg'],
-                                    texToSvg: mjx_loader['texToSvg']
-                                }, tex_str_arr);
-                            }
-                        }
-
-                        current_task_func = async function(tex_str_arr) {
-                            const render_count = `${loader_name}_render_count`;
-                            const render_limit = `${loader_name}_render_limit`;
-                            let pre_limit_tex = tex_str_arr.slice(0, mjx_loader[render_limit] - mjx_loader[render_count]);
-                            let post_limit_tex = tex_str_arr.slice(mjx_loader[render_limit] - mjx_loader[render_count]);
-                            let rendered_output_arr = []; 
-
-                            do {
-                                if (pre_limit_tex.length > 0) {
-                                    rendered_output_arr = rendered_output_arr.concat(
-                                        await typesetFunc(pre_limit_tex)
-                                    );
-
-                                    mjx_loader[render_count] += pre_limit_tex.length;
-                                    pre_limit_tex.length = 0
-                                }
-
-                                if (post_limit_tex.length > 0) {
-                                    await destroyAndRestartLoader(loader_name);
-                                    mjx_loader[render_count] = 0;
-                                    
-                                    pre_limit_tex = post_limit_tex.splice(0, Math.min(mjx_loader[render_limit], post_limit_tex.length));
-                                }
-                            } while (pre_limit_tex.length > 0);
-
-                            return rendered_output_arr;
-                        }
+                    if (['texToChtml', 'texToSvg', 'texToImg','loadSvgComponents', 'loadChtmlComponents'].includes(accessed_prop_name)) {
+                        current_task_func = this.__getTaskFunc__(accessed_prop_name, mjx_loader);
                     }
-                    else if (['loadSvgComponents', 'loadChtmlComponents'].includes(accessed_prop_name)) {
-                        const svg_or_chtml = accessed_prop_name.slice(4, 9).toLowerCase();
-
-                        current_task_func = async function(component_paths) {
-                            await newIframeMessagePromise(iframes[svg_or_chtml], {
-                                message_type: 'load_mjx_components',
-                                component_paths: component_paths
-                            });
-
-                            if (!Array.isArray(iframes[svg_or_chtml].loaded_extensions)) {
-                                iframes[svg_or_chtml].loaded_extensions = [];
-                            }
-
-                            iframes[svg_or_chtml].loaded_extensions.push(...component_paths)
+                    else if (accessed_prop_name === 'typesetPromise') {
+                        const typesetFunc = mjx_loader[accessed_prop_name].bind({resolveTexFunc: (format) => this.__getTaskFunc__(`texTo${format.charAt(0).toUpperCase()}${format.slice(1)}`, mjx_loader)});
+                        
+                        current_task_func = async function(...args) {
+                            return await typesetFunc(...args);
                         }
                     }
                     else {
