4c29a11e6633800a5d233a72e7057dc6afb9227c
daniel-153 <danieltrin1664@gmail.com>
2025-10-09 01:16:47 -0600

    patch issue with mathjax and hash scroll on page load

--- scripts/pages/info.js
+++ scripts/pages/info.js
@@ -1,3 +1,11 @@
 import "../math-jax/interface.js";
 
 mjx_loader.typesetPromise();
+
+document.getElementById('main-contents-list').addEventListener('click', (event) => {
+    if (event.target.matches('li')) {
+        document.getElementById(
+            event.target.getAttribute('data-scrollto-id')
+        ).scrollIntoView();
+    }
+});
