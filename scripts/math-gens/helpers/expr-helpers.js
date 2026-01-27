77dd2f6c4ee3ad1314c11a00c94dd031ba57cf71
daniel-153 <danieltrin1664@gmail.com>
2026-01-26 23:20:26 -0700

    extract types relevant to genSecOrd

--- scripts/math-gens/helpers/expr-helpers.js
+++ scripts/math-gens/helpers/expr-helpers.js
@@ -182,7 +182,7 @@ export const Coef = class extends Unknown {
                 else repr = this.value;
             }
             else if (this.#has_symbol) repr = this.#symbol;
-            else repr = this;
+            else repr = 'C_{?}';
 
             return String(repr)
         }
@@ -369,5 +369,295 @@ export const Pow = class extends Oper {
             }
             else throw new Error('Could not perform Pow; unevaluated or invalid operand types.');
         }
+    }
+}
+
+export const ArrayLikeExpr = class {
+    #length; #valid_types; #typeIsValid;
+    
+    constructor(valid_types) {
+        if (arguments.length >= 1) {
+            if (
+                Array.isArray(arguments[0]) &&
+                arguments[0].every(type => typeof(type) === 'function' || typeof(type) === 'string')
+            ) this.#valid_types = [...arguments[0]];
+            else throw new Error('Provided valid_types is not an array of constructors.')
+        }
+        else this.#valid_types = [Object, 'number', 'string', 'boolean', 'undefined'];
+        
+        this.#typeIsValid = function(value) {
+            return this.#valid_types.some(type_indicator => (
+                    typeof(type_indicator) === 'function' && 
+                    value instanceof type_indicator
+                ) ||
+                (
+                    typeof(type_indicator) === 'string' &&
+                    typeof(value) === type_indicator
+            ));
+        }
+        
+        if (arguments.length >= 2) {
+            for (let i = 1; i < arguments.length; i++) {
+                if (this.#typeIsValid(arguments[i])) this[i] = arguments[i];
+                else throw new Error(`Provided ArrayLikeExpr arg at idx[${i}] is not in valid types.`);
+            }
+            this.#length = arguments.length - 1;
+        }
+        else this.#length = 0;
+
+        Object.freeze(this);
+    }
+
+    *[Symbol.iterator]() {
+        for (let i = 0; i < this.#length; i++) {
+            yield this[i];
+        }
+    }
+    get length() {return this.#length}
+
+    toString() {
+        let acc_str = '';
+        for (let i = 0; i < this.length; i++) {
+            let el_str;
+            if (typeof(this.elToString) === 'function') el_str = this.elToString(i, ...arguments);
+            else if (typeof(this[i]?.toString) === 'function') el_str = this[i].toString();
+            else el_str = String(this[i]); 
+
+            if (el_str !== '0') {
+                if (
+                    acc_str.length > 0 && 
+                    el_str.length > 0 && 
+                    el_str.charAt(0) !== '-'
+                ) acc_str += `+${el_str}`;
+                else acc_str += el_str;
+            }
+        }
+
+        return acc_str.length > 0? acc_str : '0';
+    }
+}
+
+export const PolynomArray = class extends ArrayLikeExpr {
+    constructor() {
+        super([Coef, Oper], ...arguments);
+    }
+
+    elToString(i, variable = 't') {
+        if (this[i] instanceof Coef) {
+            let coef_str = this[i].toString();
+            if (coef_str === '0') return '0';
+            else if (i === 0) return coef_str;
+            else {
+                if (coef_str === '1') coef_str = '';
+                else if (coef_str === '-1') coef_str = '-';
+
+                return `${coef_str}${variable}${i > 1? `^{${i}}` : ''}`;
+            }
+        }
+        else throw new Error('Failed to convert polynom element to string: element is an operation.');
+    }
+
+    exprAtZero() {
+        return new Mul(new Int(1), this[0]);
+    }
+
+    static scale(polynom_arr, scalar) {
+        if (!(polynom_arr instanceof PolynomArray)) {
+            throw new Error('PolynomArray.scale: first argument must be a PolynomArray.');
+        }
+        if (!(scalar instanceof Value || scalar instanceof Coef || scalar instanceof Oper)) {
+            throw new Error('PolynomArray.scale: second argument must be a Value, Coef, or Oper.');
+        }
+        return new PolynomArray(...Array.from(polynom_arr, coef => new Mul(scalar, coef)));
+    }
+
+    static add(polynom_arr1, polynom_arr2) {
+        if (!(polynom_arr1 instanceof PolynomArray)) {
+            throw new Error('PolynomArray.add: first argument must be a PolynomArray.');
+        }
+        if (!(polynom_arr2 instanceof PolynomArray)) {
+            throw new Error('PolynomArray.add: second argument must be a PolynomArray.');
+        }
+        const [shorter_poly, longer_poly] = polynom_arr2.length > polynom_arr1.length? [polynom_arr1, polynom_arr2] : [polynom_arr2, polynom_arr1];
+
+        const sum_poly = [];
+        for (let i = 0; i < longer_poly.length; i++) {
+            const long_term = longer_poly[i];
+            const short_term = i < shorter_poly.length? shorter_poly[i] : new Coef([Int], new Int(0));
+            sum_poly.push(new Sum(short_term, long_term));
+        }
+
+        return new PolynomArray(...sum_poly);
+    }
+
+    static diff(polynom_arr) {
+        if (!(polynom_arr instanceof PolynomArray)) {
+            throw new Error('PolynomArray.diff: argument must be a PolynomArray.');
+        }
+        const diffed_poly = [];
+
+        for (let n = 1; n < polynom_arr.length; n++) {
+            diffed_poly.push(new Mul(new Int(n), polynom_arr[n]));
+        }
+
+        return new PolynomArray(...diffed_poly);
+    }
+};
+
+export const PolExpTrig = class {
+    #pet_values = {};
+    
+    constructor(pet_obj) {
+        if (arguments.length <= 1) {
+            const proposed_obj = arguments.length === 1? arguments[0] : {};
+            if (proposed_obj instanceof Object) {
+                ['degree', 'trig_freq', 'exp_freq'].forEach(pet_value => {
+                    if (Object.prototype.hasOwnProperty.call(proposed_obj, pet_value)) {
+                        if (pet_value === 'degree') {
+                            if (proposed_obj[pet_value] instanceof Int && proposed_obj[pet_value].value >= 0) {
+                                this.#pet_values[pet_value] = proposed_obj[pet_value];
+                            }
+                            else throw new Error('Invalid degree, PolExpTrig degree must be a non-negative Int.');
+                        }
+                        else if (proposed_obj[pet_value] instanceof Int || proposed_obj[pet_value] instanceof Frac) {
+                            this.#pet_values[pet_value] = proposed_obj[pet_value];
+                        }
+                        else throw new Error(`Invalid ${pet_value}, PolExpTrig ${pet_value} must be an Int or Frac.`);
+                    }
+                    else this.#pet_values[pet_value] = new Int(0);
+                });
+
+                ['polynom_c', 'polynom_s'].forEach(pet_value => {
+                    if (Object.prototype.hasOwnProperty.call(proposed_obj, pet_value)) {
+                        if (
+                            proposed_obj[pet_value] instanceof PolynomArray && 
+                            proposed_obj[pet_value].length === this.#pet_values.degree.value + 1
+                        ) this.#pet_values[pet_value] = proposed_obj[pet_value];
+                        else throw new Error(`Provided ${pet_value} is not a polynom, or its degree differs from the pet degree.`);
+                    }
+                    else this.#pet_values[pet_value] = new PolynomArray(...Array.from(
+                        {length: this.#pet_values.degree.value + 1},
+                        () => new Coef([Int, Frac])
+                    ));
+                });
+            }
+            else throw new Error(`Invalid argument to PolExpTrig, typeof '${typeof(proposed_obj)}'.`);
+        }
+        else throw new Error('PolExpTrig constructor must recieve 0 or 1 arguments.');
+    }
+
+    get degree() {return this.#pet_values.degree}
+    get trig_freq() {return this.#pet_values.trig_freq}
+    get exp_freq() {return this.#pet_values.exp_freq}
+    get polynom_c() {return this.#pet_values.polynom_c}
+    get polynom_s() {return this.#pet_values.polynom_s}
+
+    toString(variable = 't') {
+        if (this.trig_freq.value !== 0) {
+            let freq_coef = this.trig_freq.toString();
+            if (freq_coef === '1') freq_coef = '';
+            else if (freq_coef === '-1') freq_coef = '-';
+
+            let sin_cos_expr = '';
+            [
+                ['sin', this.polynom_s], 
+                ['cos', this.polynom_c]
+            ].forEach(func_pol_pair => {
+                const [func_str, polynom_arr] = func_pol_pair;
+                let polynom_str = polynom_arr.toString(variable);
+
+                if (polynom_str === '0') return;
+                else {
+                    let curr_trig_term;
+                    if (Array.from(polynom_arr).filter(coef => coef.value?.value !== 0).length === 1) {
+                        let t_n_coef = polynom_str.split(variable)[0];
+                        polynom_str = polynom_str.replace(t_n_coef, '');
+                        if (t_n_coef === '1') t_n_coef = '';
+                        else if (t_n_coef === '-1') t_n_coef = '-';
+
+                        curr_trig_term = `${t_n_coef}${polynom_str}\\${func_str}(${freq_coef}${variable})`;
+                    }
+                    else {
+                        curr_trig_term = `(${polynom_str})\\${func_str}(${freq_coef}${variable})`;
+                    }
+
+                    if (sin_cos_expr !== '' && curr_trig_term.charAt(0) !== '-') {
+                        sin_cos_expr += `+${curr_trig_term}`;
+                    }
+                    else sin_cos_expr += curr_trig_term;
+                }
+            });
+
+            if (sin_cos_expr === '') return '0';
+            else if (this.exp_freq.value !== 0) {
+                let exp_coef = this.exp_freq.toString();
+                if (exp_coef === '1') exp_coef = '';
+                else if (exp_coef === '-1') exp_coef = '-';
+
+                const exp_expr = `e^{${exp_coef}${variable}}`;
+                
+                const includes_sin = sin_cos_expr.includes('\\sin');
+                const includes_cos = sin_cos_expr.includes('\\cos');
+
+                if (includes_sin && includes_cos) {
+                    return `${exp_expr}(${sin_cos_expr})`;
+                }
+                else {
+                    const included_trig = includes_sin? '\\sin' : '\\cos';
+                    const [pre_trig, post_trig] = sin_cos_expr.split(included_trig, 2);
+                    const trig_alone = `${included_trig}${post_trig}`;
+
+                    return `${pre_trig}${exp_expr}${trig_alone}`;
+                }
+            }
+            else return sin_cos_expr;
+        }
+        else {
+            let polynom_str = this.polynom_c.toString(variable);
+
+            if (polynom_str === '0') return '0';
+            else if (this.exp_freq.value !== 0) {
+                let exp_coef = this.exp_freq.toString();
+                if (exp_coef === '1') exp_coef = '';
+                else if (exp_coef === '-1') exp_coef = '-';
+
+                const exp_expr = `e^{${exp_coef}${variable}}`;
+
+                if (Array.from(this.polynom_c).filter(coef => coef.value?.value !== 0).length === 1) {
+                    let t_n_coef = polynom_str.split(variable)[0];
+                    polynom_str = polynom_str.replace(t_n_coef, '');
+                    if (t_n_coef === '1') t_n_coef = '';
+                    else if (t_n_coef === '-1') t_n_coef = '-';
+
+                    return `${t_n_coef}${polynom_str}${exp_expr}`;
+                }
+                else {
+                    return `${exp_expr}(${polynom_str})`;
+                }
+            }
+            else return polynom_str;
+        }
+    }
+
+    exprAtZero() {
+        return this.#pet_values.polynom_c.exprAtZero();
+    }
+}
+
+export const PolExpTrigArray = class extends ArrayLikeExpr {
+    constructor() {
+        super([PolExpTrig], ...arguments);
+    }
+
+    elToString(i, variable = 't') {
+        return this[i].toString(variable);
+    }
+
+    exprAtZero() {
+        let acc_expr = new Sum(new Int(0), new Int(0));
+        for (let i = 0; i < this.length; i++) {
+            acc_expr = new Sum(acc_expr, this[i].exprAtZero());
+        }
+        return acc_expr;
     }
 }
